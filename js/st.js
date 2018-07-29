72047c57fe4cb23de2ccd8cf13a2092e718f3f0b
Ashadullah Shawon <shawonashadullah@gmail.com>
2018-07-29 03:37:58 -0700

    Add files via upload

---
+++ js/st.js
@@ -0,0 +1,90 @@
+
+$(document).ready(function(){
+    $.ajax({
+
+    	url:"http://localhost/roj/profile.php?user=shawon",
+    	method:"GET",
+      dataType: "json",
+    	success:function(data){
+               //console.log(data);
+               
+
+               var verdicts=[];
+               var ac=[];
+               var wa=[];
+               var tle=[];
+               var obj;
+               
+
+              console.log(data);
+
+              verdicts.push("User's Statistics");
+              //verdicts.push("Wrong Answer");
+              //verdicts.push("Time Limit Exceed");
+
+              
+              
+              //console.log(data);
+              
+
+               
+               	  
+              ac.push(data[0].verdict);
+              wa.push(data[1].verdict);
+              tle.push(data[2].verdict);
+                   //console.log(data[i].verdict);
+
+               
+               
+               
+
+               
+
+               var chartdata={
+               	    labels:verdicts,
+               	    datasets:[
+                      {
+                      	  label:'AC',
+                      	  backgroundColor:'green',
+                      	  borderColor:'green',
+                      	  hoverBackgroundColor:'green',
+                      	  data:ac,
+                      },
+                      
+                      {
+                          label:'WA',
+                          backgroundColor:'red',
+                          borderColor:'red',
+                          hoverBackgroundColor:'red',
+                          data:wa,
+                      },
+                      
+                      {
+                          label:'TLE',
+                          backgroundColor:'blue',
+                          borderColor:'blue',
+                          hoverBackgroundColor:'blue',
+                          data:tle,
+                      }
+
+
+
+               	    ]
+               };
+
+               var ctx=$('#mycanvas');
+               var barGraph=new Chart(ctx,{
+
+                     type:'bar',
+                     responsive:true,
+                     data:chartdata
+
+               });
+    	},
+    	error:function(data){
+    		console.log(data);
+    	}
+
+    });
+
+});
